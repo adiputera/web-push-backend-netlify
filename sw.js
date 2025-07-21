64dfe7abdf82f94af55fd2c7282d615330c665cb
Yusuf F. Adiputera <yfa.adiputera@gmail.com>
2025-07-22 00:42:25 +0700

    Remove Sw.JS

--- sw.js
+++
@@ -1,9 +0,0 @@
-self.addEventListener('push', function (event) {
-    const data = event.data?.json() || {};
-    event.waitUntil(
-        self.registration.showNotification(data.title || "Hello!", {
-            body: data.body || "You have a new notification!",
-            icon: "/icon.png"
-        })
-    );
-});
